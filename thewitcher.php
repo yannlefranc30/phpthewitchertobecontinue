ff0d181e0c41b06ccc365dc7efff3243c15cf555
yann <yannlefranc330@gmail.com>
2023-05-23 01:46:47 +0200

    je suis un dev

---
+++ thewitcher.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+
+<?php
+$thewitcher = "red";
+
+switch ($thewitcher) {
+    case "red":
+        echo "ta couleur est  rouge";
+        break;
+    case "blue":
+        echo "ta couleur est bleue";
+        break;
+    case "green":
+        echo "ta couleur est vert";
+        break;
+    default:
+        echo "bonjour";
+}
+?>
+</body>
+</html>
